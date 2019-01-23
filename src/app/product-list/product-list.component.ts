ded9f3495f810118d190f10bc78924c7d64c95f3
bhasani <44534315+bhasani@users.noreply.github.com>
2019-01-23 13:21:00 -0600

    part6

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+import { k } from '@angular/core/src/render3';
+import { jsonpFactory } from '@angular/http/src/http_module';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products: Product[];
+
+  constructor(private _productService: ProductService) { }
+
+  ngOnInit() {
+    this._productService.getProducts().subscribe(response =>
+      this.products = response);
+  }
+
+}
